5d28bc16434bdd3c3629bbc8e233340ab51d160f
Ashish Gaikwad <ashish@letscooee.com>
2022-06-14 11:24:48 +0530

    Update Code comment's for better understanding

--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,34 @@ import 'package:cooee_plugin/cooee_plugin.dart';
 
 void main() {
   runApp(const MyApp());
+
+  //  Cooee Specific- Register for CooeeInAppNotificationAction.
   CooeePlugin cooeePlugin = CooeePlugin();
   cooeePlugin.setCooeeInAppNotificationAction(inAppTriggered);
 }
 
-/// Will listen for @invokeMethod which will triggered by Java SDK
+/// Will listen for @invokeMethod which will triggered by Cooee Plugin.
 void inAppTriggered(Map<String, dynamic> map) {
-  // map["actionType"] varies according to app domain/event
+  // Cooee Specific- map["actionType"] varies according to app App domain/event.
   if (map["actionType"] == "PLAY_COURSE") {
-    // Product id will be available via "map["courseID"]"
+    // Cooee Specific- Product id will be available via "map["courseID"]"
   } else if (map["actionType"] == "APPLY_COUPON") {
     // Your code to apply a given coupon.
-    // Coupon code can be accessed via "map["couponCode"]"
+    // Cooee Specific- Coupon code can be accessed via "map["couponCode"]"
   } else if (map["actionType"] == "GO_TO_SCREEN") {
     // Your code to take user to a particular screen
-    // Screen name can be accessed via "map["screenName"]"
+    // Cooee Specific- Screen name can be accessed via "map["screenName"]"
   }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -49,15 +41,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -65,51 +48,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             OutlinedButton(
@@ -124,23 +70,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: const Text('Update User profile')),
             OutlinedButton(
                 onPressed: () => {
-                  CooeePlugin.setCurrentScreen("Home"),
-                },
+                      CooeePlugin.setCurrentScreen("Home"),
+                    },
                 child: const Text('Tack current screen')),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
   /// Send event to Cooee
   void sendEvent() {
-    // event name and event propertied can be varied according to app domain
+    // Cooee Specific- event name and event propertied can be varied according to app domain
     var eventProperties = {
       "item": {
         "id": "1",
@@ -156,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   /// Update user profile
   void updateUserProfile() {
-    // You can pass any data like user_referral_code, user_id, user_name, etc.
+    // Cooee Specific- You can pass any data like user_referral_code, user_id, user_name, etc.
     var userProfile = {
       "name": "test-user",
       "email": "test@letscooee.com",
